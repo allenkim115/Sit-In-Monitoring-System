c9b7fc6389e100bcb548addc0348188bc5a5ef2b
Allen Kim <allenkimrafaela@gmail.com>
2025-04-08 02:55:18 +0800

    update db

--- db/sitin.sql
+++ db/sitin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2025 at 04:18 PM
+-- Generation Time: Apr 07, 2025 at 08:54 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -63,6 +63,31 @@ INSERT INTO `announcement` (`ID`, `TITLE`, `MESSAGE`, `TIMESTAMP`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `ID` int(11) NOT NULL,
+  `SITIN_RECORD_ID` int(11) NOT NULL,
+  `STUDENT_ID` varchar(50) NOT NULL,
+  `RATING` int(1) NOT NULL,
+  `COMMENT` text DEFAULT NULL,
+  `CREATED_AT` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`ID`, `SITIN_RECORD_ID`, `STUDENT_ID`, `RATING`, `COMMENT`, `CREATED_AT`) VALUES
+(1, 41, '2000', 5, 'nice', '2025-04-07 16:41:32'),
+(2, 38, '2000', 1, 'meh', '2025-04-07 16:44:49'),
+(3, 40, '20951505', 5, 'good experience', '2025-04-07 16:45:32'),
+(4, 39, '20951505', 1, 'rude', '2025-04-07 16:47:04');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `sitin_records`
 --
 
@@ -80,23 +105,7 @@ CREATE TABLE `sitin_records` (
 --
 
 INSERT INTO `sitin_records` (`ID`, `IDNO`, `PURPOSE`, `LABORATORY`, `TIME_IN`, `TIME_OUT`) VALUES
-(2, 3000, 'C#', '526', '2025-03-13 12:02:52', '2025-03-13 12:44:13'),
 (5, 20951505, 'PHP Programming', '524', '2025-03-13 12:39:29', '2025-03-13 12:39:46'),
-(6, 2000, 'C++', '530', '2025-03-13 12:42:34', '2025-03-13 12:42:40'),
-(7, 2000, 'PHP', '524', '2025-03-20 00:15:30', '2025-03-20 00:23:32'),
-(8, 2000, 'PHP', '524', '2025-03-20 00:28:40', '2025-03-20 00:29:19'),
-(9, 3123052, 'PHP', '524', '2025-03-20 00:30:48', '2025-03-20 00:30:56'),
-(10, 2000, 'PHP', '524', '2025-03-20 00:36:08', '2025-03-20 00:52:19'),
-(11, 2000, 'PHP', '524', '2025-03-20 01:05:13', '2025-03-20 01:08:43'),
-(12, 2000, 'PHP', '524', '2025-03-20 01:12:16', '2025-03-20 01:19:55'),
-(13, 3000, 'PHP', '524', '2025-03-20 01:14:37', '2025-03-20 01:39:12'),
-(14, 3123052, 'PHP', '524', '2025-03-20 01:17:23', '2025-03-20 01:39:14'),
-(15, 20951505, 'PHP', '524', '2025-03-20 01:19:31', '2025-03-20 01:39:16'),
-(16, 2000, 'PHP', '524', '2025-03-20 01:20:06', '2025-03-20 01:21:22'),
-(17, 2000, 'PHP', '524', '2025-03-20 01:21:29', '2025-03-20 01:22:55'),
-(18, 2000, 'PHP', '524', '2025-03-20 01:23:07', '2025-03-20 01:23:14'),
-(19, 2000, 'PHP', '524', '2025-03-20 01:23:50', '2025-03-20 01:23:59'),
-(20, 2000, 'PHP', '524', '2025-03-20 01:32:08', '2025-03-20 01:38:41'),
 (21, 2000, 'PHP Programming', '524', '2025-03-20 01:38:52', '2025-03-20 01:39:08'),
 (22, 2000, 'C Programming', '526', '2025-03-20 01:39:27', '2025-03-20 01:43:18'),
 (23, 2000, 'PHP Programming', '524', '2025-03-20 01:43:56', '2025-03-25 22:11:30'),
@@ -116,7 +125,8 @@ INSERT INTO `sitin_records` (`ID`, `IDNO`, `PURPOSE`, `LABORATORY`, `TIME_IN`, `
 (37, 20951505, '.Net Programming', '528', '2025-03-25 23:20:24', '2025-03-26 00:08:15'),
 (38, 2000, 'C++ Programming', '526', '2025-03-26 22:08:14', '2025-03-26 22:09:35'),
 (39, 20951505, 'Python Programming', '530', '2025-03-26 22:09:16', '2025-03-26 22:09:33'),
-(40, 20951505, 'PHP Programming', '528', '2025-03-26 22:09:55', '2025-03-26 23:17:44');
+(40, 20951505, 'PHP Programming', '528', '2025-03-26 22:09:55', '2025-03-26 23:17:44'),
+(41, 2000, 'C++ Programming', '526', '2025-04-08 00:25:57', '2025-04-08 00:26:08');
 
 -- --------------------------------------------------------
 
@@ -143,10 +153,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`ID`, `IDNO`, `LASTNAME`, `FIRSTNAME`, `MIDDLENAME`, `COURSE`, `YEAR_LEVEL`, `USERNAME`, `PASSWORD`, `PROFILE_PIC`, `SESSION_COUNT`) VALUES
-(2, 2000, 'Doe', 'John', '', 'BSCS', 2, 'j.doe', '$2y$10$OECVWvoX5l1zuQSXmz/LjO.Wf3t70WTI3DRb9y.TG3mbE9TLdeTo.', 'uploads/67cfba7e8a091_man-removebg-preview.png', 28),
-(4, 3000, 'Doe', 'Jake', '', 'BSIT', 1, 'jake123', '$2y$10$.kySdo9lb7URfqAf8VZTSOyVEOfNT9aUQFZsyfHxX5T2tOIEyg0CC', 'images/default_pic.png', 27),
-(3, 3123052, 'Dela Cruz', 'Juan', '', 'BSCpE', 1, 'j.dela_cruz', '$2y$10$Uo.LAXxgNqAZB3zYw88TJe2Dtd7ZzAmqb53oU5N7EDl6JccM36I/u', 'uploads/67cfba1b81d48_meme-gif-pfp-9.gif', 26),
-(1, 20951505, 'Rafaela', 'Allen Kim', 'Calaclan', 'BSIT', 3, 'allenkim115', '$2y$10$2vP9w6uLJVF7f06XwKYqcOI0wsv3ZdIcOy5F.a8mwTZGa5iIHZNDe', 'uploads/67c929f4c620b_icegif-6567.gif', 28);
+(2, 2000, 'Doe', 'John', '', 'BSCS', 2, 'j.doe', '$2y$10$OECVWvoX5l1zuQSXmz/LjO.Wf3t70WTI3DRb9y.TG3mbE9TLdeTo.', 'uploads/67cfba7e8a091_man-removebg-preview.png', 29),
+(4, 3000, 'Doe', 'Jake', '', 'BSIT', 1, 'jake123', '$2y$10$.kySdo9lb7URfqAf8VZTSOyVEOfNT9aUQFZsyfHxX5T2tOIEyg0CC', 'images/default_pic.png', 30),
+(3, 3123052, 'Dela Cruz', 'Juan', '', 'BSCpE', 1, 'j.dela_cruz', '$2y$10$Uo.LAXxgNqAZB3zYw88TJe2Dtd7ZzAmqb53oU5N7EDl6JccM36I/u', 'uploads/67cfba1b81d48_meme-gif-pfp-9.gif', 30),
+(1, 20951505, 'Rafaela', 'Allen Kim', 'Calaclan', 'BSIT', 3, 'allenkim115', '$2y$10$2vP9w6uLJVF7f06XwKYqcOI0wsv3ZdIcOy5F.a8mwTZGa5iIHZNDe', 'uploads/67c929f4c620b_icegif-6567.gif', 30);
 
 --
 -- Indexes for dumped tables
@@ -163,6 +173,13 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `announcement`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `SITIN_RECORD_ID` (`SITIN_RECORD_ID`);
 
 --
 -- Indexes for table `sitin_records`
@@ -195,10 +212,16 @@ ALTER TABLE `announcement`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `sitin_records`
 --
 ALTER TABLE `sitin_records`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -209,6 +232,12 @@ ALTER TABLE `user`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`SITIN_RECORD_ID`) REFERENCES `sitin_records` (`ID`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `sitin_records`
